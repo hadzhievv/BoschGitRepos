c08bda38ac7c8914bbad0b88b70cf65c0bd9b627
Malin Hadzhiev <malin.hadjiev@abv.bg>
2022-11-04 16:43:00 +0200

    Initial commit

---
+++ src/main/java/com/malin/bosch/BoschProjectApplication.java
@@ -0,0 +1,13 @@
+package com.malin.bosch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malin/bosch/BoschProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malin.bosch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoschProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
